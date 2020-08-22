7e3f97a245016742e281d1c0f288986be0ef7be1
Pri Lopes <cillaeslopes@gmail.com>
2020-08-22 14:29:55 -0300

    [feature] swagger

--- src/routes/default.router.ts
+++ src/routes/default.router.ts
@@ -1,13 +1,13 @@
 import Router from 'express'
 import * as healthCheckController from '../health/health-check.controller'
-// import * as swaggerUi from 'swagger-ui-express'
+import * as swaggerUi from 'swagger-ui-express'
 
-// import { swaggerDocument } from '../swagger/swagger'
+import { swaggerDocument } from '../swagger/swagger'
 
 const defaultRouter = Router()
 defaultRouter.get('/health', healthCheckController.check)
 
-// defaultRouter.use('/api-docs', swaggerUi.serve)
-// defaultRouter.get('/api-docs', swaggerUi.setup(swaggerDocument))
+defaultRouter.use('/docs', swaggerUi.serve)
+defaultRouter.get('/docs', swaggerUi.setup(swaggerDocument))
 
 export default defaultRouter
---
+++ src/swagger/swagger.ts
@@ -0,0 +1,10 @@
+export const swaggerDocument = {
+  openapi: '3.0.1',
+  info: {
+    version: '1.0.0',
+    title: 'Innovate API',
+    description: 'Innovate website API',
+  },
+  tags: [],
+  paths: {},
+}
