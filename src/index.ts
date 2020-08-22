c2ae81d8fc69ed3da2857697bf612e39bcff5b6c
Priscilla Lopes <prilopes@Pris-MacBook-Pro.local>
2020-08-22 13:14:50 -0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+dotenv.config();
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
