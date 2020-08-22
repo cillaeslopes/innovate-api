ba52dc677a8d456f1c26d55094d4e8d10c515d77
Pri Lopes <cillaeslopes@gmail.com>
2020-08-22 13:58:18 -0300

    add eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import * as dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
+import * as dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
 
-dotenv.config();
+dotenv.config()
 
-const PORT: number = parseInt(process.env.PORT as string, 10);
+const PORT: number = parseInt(process.env.PORT as string, 10)
 
-const server = express();
+const server = express()
 
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+  console.log(`Listening on port ${PORT}`)
+})
